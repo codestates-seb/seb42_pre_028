5a36d0e9a5ab1b221b7007cd9aea24b3df9d1807
CARRO11 <eleying@naver.com>
2023-02-17 10:33:29 +0900

    exeception handler 초안 + moretodo

--- server/src/main/java/Preproject28/server/exception/ExceptionCode.java
+++ server/src/main/java/Preproject28/server/exception/ExceptionCode.java
@@ -3,13 +3,13 @@ package Preproject28.server.exception;
 import lombok.Getter;
 
 public enum ExceptionCode {
-    MEMBER_NOT_FOUND(404, "Member not found"),
+    MEMBER_NOT_FOUND(404, ",Member not found"),
     MEMBER_EXISTS(409, "Member exists"),
-    COFFEE_NOT_FOUND(404, "Coffee not found"),
-    COFFEE_CODE_EXISTS(409, "Coffee Code exists"),
-    ORDER_NOT_FOUND(404, "Order not found"),
-    CANNOT_CHANGE_ORDER(403, "Order can not change"),
-    NOT_IMPLEMENTATION(501, "Not Implementation"),
+    QUESTION_NOT_FOUND(404, "Question not found"),
+   QUESTION_EXISTS(404, "Question exists"),
+    ANSWER_NOT_FOUND(404, "Answer not found"),
+    ANSWER_CHANGE_ORDER(403, "Answer exists"),
+    NOT_IMPLEMENTATION(501, "No Implementation"),
     INVALID_MEMBER_STATUS(400, "Invalid member status");
 
     @Getter
