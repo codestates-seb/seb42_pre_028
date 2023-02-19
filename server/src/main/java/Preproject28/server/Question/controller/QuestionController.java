d8626a281f48a0f018a65bcbfaaafd5e46640624
CARRO11 <eleying@naver.com>
2023-02-20 08:08:12 +0900

    Answer 엔티티 -> Controller 제외

--- server/src/main/java/Preproject28/server/Question/controller/QuestionController.java
+++ server/src/main/java/Preproject28/server/Question/controller/QuestionController.java
@@ -1,11 +1,40 @@
 package Preproject28.server.Question.controller;
 
+import Preproject28.server.Question.dto.QuestionPostDto;
+import Preproject28.server.Question.dto.QuestionResponseDto;
+import Preproject28.server.Question.entity.Question;
+import Preproject28.server.Question.mapper.QuestionMapper;
+import Preproject28.server.Question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/question")
 @RequiredArgsConstructor
+@Validated
 public class QuestionController {
+    private QuestionService questionService;
+
+    private QuestionMapper questionMapper;
+
+    public QuestionController (QuestionService questionService, QuestionMapper questionMapper){
+        this.questionMapper = questionMapper;
+        this.questionService = questionService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity postQuestion(@Valid @RequestBody QuestionPostDto questionPostDto){
+        Question question = questionService.createQuestion(questionMapper.questionPostDtoToQuestion(questionPostDto));
+        QuestionResponseDto response = questionMapper.questionToQuestionResponseDto(question);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 }
