bf4d73dcf9488a70b9447610a5f904acdf37b622
Minogod <93550624+Minogod@users.noreply.github.com>
2023-02-26 00:07:12 +0900

    병합중

---
+++ server/build/generated/sources/annotationProcessor/java/main/Preproject28/server/answerVote/mapper/AnswerVoteMapperImpl.java
@@ -0,0 +1,13 @@
+package Preproject28.server.answerVote.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-02-25T23:59:32+0900",
+    comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.jar, environment: Java 11.0.17 (Azul Systems, Inc.)"
+)
+@Component
+public class AnswerVoteMapperImpl implements AnswerVoteMapper {
+}
